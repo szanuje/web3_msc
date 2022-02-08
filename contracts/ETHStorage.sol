c8c4705863fb16c37b1f7e39761e7c32d4fcbace
maksple <maksple1337@gmail.com>
2022-02-08 21:25:11 +0100

    ui cleanup

--- contracts/ETHStorage.sol
+++ contracts/ETHStorage.sol
@@ -7,15 +7,13 @@ pragma solidity >=0.7.0 <0.9.0;
  * @dev Store & retrieve ETH
  */
 contract ETHStorage {
-
     mapping(address => uint256) private addressToAmountFunded;
 
     function fund() public payable {
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function getBalance() public view returns(uint256) {
+    function getBalance() public view returns (uint256) {
         return addressToAmountFunded[msg.sender];
     }
-
 }
