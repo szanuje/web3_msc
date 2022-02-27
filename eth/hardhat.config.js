ca9385f540b251ab0dee4f8f0c7ff6533ffe96f0
maksple <maksple1337@gmail.com>
2022-02-27 21:34:03 +0100

    fix: change network name

--- eth/hardhat.config.js
+++ eth/hardhat.config.js
@@ -8,7 +8,7 @@ const RINKEBY_PRIVATE_KEY = "";
 module.exports = {
   solidity: "0.8.10",
   networks: {
-    ropsten: {
+    rinkeby: {
       url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
       accounts: [`${RINKEBY_PRIVATE_KEY}`],
     },
