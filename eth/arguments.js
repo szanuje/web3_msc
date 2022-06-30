1ddbbf78e3360c67ad882348ab557e1697aab3ba
maksple <maksple1337@gmail.com>
2022-06-30 11:49:08 +0200

    feat: etherscan verification parameters

---
+++ eth/arguments.js
@@ -0,0 +1,7 @@
+// constructor arguments used to create MainContract,
+// needed for Etherscan verification
+module.exports = [
+  "0x12d672ff9bfc212000c2976259b2c3de63e0ab3e",
+  "0x5a2c95f45754881ec783ec8452fa526a8b6b0de1",
+  "0x19c33398f43be4af3058b6a5ced9ec46da400c7f",
+];
